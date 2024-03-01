5c80ff5fcf914c0399a523e910905d80e0366064
Desenvolvimento <dsadm@educ501.local>
2024-02-29 21:30:16 -0300

    first done

---
+++ first_exercise/android/app/src/main/kotlin/com/select/folder/first_exercise/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.select.folder.first_exercise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ first_exercise/lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [NavigationBar].
+
+void main() => runApp(const NavigationBarApp());
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const NavigationExample(),
+    );
+  }
+}
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.factory),
+            icon: Icon(Icons.factory_outlined),
+            label: 'Industría',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.agriculture),
+            icon: Icon(Icons.agriculture_outlined),
+            label: 'Agricultura',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.local_hospital_outlined),
+            icon: Icon(Icons.local_hospital),
+            label: 'Saúde',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.money_outlined),
+            icon: Icon(Icons.money),
+            label: 'Ecônomia',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        /// Home page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Indústria',
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+
+        /// agriculture page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Agricultura',
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+
+        /// health page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Saúde',
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+
+        /// Economy page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Ecônomia',
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+      ][currentPageIndex],
+    );
+  }
+}
